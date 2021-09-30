823422aed4861d25b10b0f35c3af6de712c94686
Daniel75_ <danielalejandrolopez.tic@ucaldas.edu.co>
2021-09-29 20:39:17 -0500

    Prueba de guardado

---
+++ Ejemplo.app/Ejemplo.app.Clases/Enumerador.cs
@@ -0,0 +1,13 @@
+namespace Ejemplo.app.Clases
+{
+    public enum Enumerador
+    {
+        gato=1,
+        perro=2,
+        pajaro=3,
+        caballo=4,
+
+        leon=5,
+        
+    }
+}
---
+++ Ejemplo.app/Ejemplo.app.Clases/Prueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejemplo.app.Clases
+{
+    public class Class1
+    {
+
+        public void ConsultarEnumerodor(){
+            string DatoGato= Enumerador.gato.ToString();
+            int i=Convert.ToInt32(Enumerador.gato);
+        }
+    }
+}
